1b9b2f299740ca37088539516cc1dc641e6c4af7
Ruben <rubenmantilladev@gmail.com>
2025-02-02 19:51:07 -0500

    feat: create header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  // { path: 'products', component: ProductListComponent },
+  // { path: 'products/:id', component: ProductDetailComponent },
+  // { path: 'cart', component: CartComponent },
+  // { path: 'checkout', component: CheckoutComponent },
+  // { path: 'orders', component: OrderListComponent },
+  // { path: 'orders/:id', component: OrderDetailComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
