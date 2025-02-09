41732c268301fbdf0208dc8f42a63ebdf7607ea0
Ruben <rubenmantilladev@gmail.com>
2025-02-08 20:59:08 -0500

    style: add responsive styles

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  @Output() openCart = new EventEmitter<void>();
 
+  onOpenCart(): void {
+    this.openCart.emit();
+  }
 }
